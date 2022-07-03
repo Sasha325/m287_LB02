34a86bfc43523ba950e4af5a0f804812333826d8
Sasha Gamba <sasha.gamba@swisscom.com>
2022-07-04 00:09:19 +0200

    updated

---
+++ kontakt.js
@@ -0,0 +1,98 @@
+const form = document.getElementById("kontakt-formular");
+
+form.addEventListener("submit", submitHandler);
+
+function submitHandler(e) {
+  e.preventDefault();
+
+  // erstellt ein formData Objekt
+  const myFormData = new FormData(e.target);
+
+  // wandelt formData in ein benutzbares object aus einträgen
+  const formDataObj = Object.fromEntries(myFormData.entries());
+
+  // wenn alle erfüllt sind dann schicke daten zum server
+
+  // Hier ist Client Validierung!
+
+  const nameIsTrue = onlyLettersSpaces(formDataObj.firstName);
+
+  if (!nameIsTrue) {
+    window.alert(
+      "Your first name does not consist only of letters. Please change!"
+    );
+    return;
+  }
+
+  const lastNameIsTrue = onlyLettersSpaces(formDataObj.lastName);
+
+  if (!lastNameIsTrue) {
+    window.alert(
+      "Your Last name does not consist only of letters. Please change!"
+    );
+    return;
+  }
+
+  const emailIsTrue = validateEmail(formDataObj.email);
+
+  if (!emailIsTrue) {
+    window.alert("Deine Email enthält kein @ Zeichen. Please change!");
+    return;
+  }
+
+  // wenn alles validiert --> schicke alle daten an den server
+  fetch("http://localhost:3000/form-validation", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      firstName: formDataObj.firstName,
+      lastName: formDataObj.lastName,
+      email: formDataObj.email,
+      message: formDataObj.message,
+      requestType: formDataObj.reason,
+    }),
+  })
+    .then((res) => res.json())
+    .then((json) => console.log(json))
+    .catch((e) => console.log(e));
+
+  console.log(formDataObj);
+}
+
+// validate functions
+function onlyLettersSpaces(str) {
+  if (/^[a-zA-Z\\s]+$/.test(str)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validateEmail(email) {
+  if (email.includes("@")) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validateMessage(message) {
+  if (message.length <= 200) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validateRequestType(type) {
+  //type: "priceRequest", "offer", "other"
+  // const acceptedRequests = ["priceRequest", "offer", "other"]
+  // return acceptedRequests.includes(type)
+  if (type === "priceRequest" || type === "offer" || type === "other") {
+    return true;
+  } else {
+    return false;
+  }
+}
